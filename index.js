519569b967043251254f868ba7253fece647af0b
ThejeshwarAB <42470612+ThejeshwarAB@users.noreply.github.com>
2019-12-22 00:35:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const ytdl = require('ytdl-core');
 
 const app = express();
-const port = process.env.port || 4000;
+const port = process.env.port;
 
 app.use(cors());
 
